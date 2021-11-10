6d73c6554b8d76b512a927649afcb8ef18659771
Bartłomiej <89859667+BarS99@users.noreply.github.com>
2021-11-10 22:33:16 +0100

    Update imagePeeler.js
    Bug fixes

--- static/scripts/imagePeeler.js
+++ static/scripts/imagePeeler.js
@@ -1,7 +1,7 @@
 class ImagePeeler {
     constructor(box, mask, settings) {
-        this.box = document.querySelector(box);
-        this.mask = document.querySelector(mask);
+        this.box = box;
+        this.mask = this.box.querySelector(mask);
         this.startCapturing = false;
         this.isMousedown = false;
         this.settings = {
@@ -49,7 +49,7 @@ class ImagePeeler {
         if (!this.startCapturing || (this.settings.onMousedown && !this.isMousedown)) return;
         e.preventDefault();
 
-        let height = (((e.clientY - (window.pageYOffset + this.box.getBoundingClientRect().top)) / this.box.getBoundingClientRect().height) * 100).toFixed(2);
+        let height = (((e.clientY - this.box.getBoundingClientRect().top) / this.box.getBoundingClientRect().height) * 100).toFixed(2);
         if (height < 0) height = 0;
         else if (height > 100) height = 100;
 
